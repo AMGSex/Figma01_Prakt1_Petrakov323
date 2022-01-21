a6ba8bfa00990f33d8e0270a961444a8d91708bb
AMGSex <maksimpetrakov25929@gmail.com>
2022-01-21 23:23:43 +0300

    Добавьте файлы проекта.

---
+++ Figma01/Figma01/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Figma01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Kartochka.Source = ImageSource.FromResource("Figma01.haip.jpeg");
+        }
+
+        private async void btnPasenis_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
